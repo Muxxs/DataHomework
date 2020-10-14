b8945c38ffc88f540781e85311cbf857f4bb01e7
Muxxs <747306970@qq.com>
2020-10-14 11:01:57 +0800

    update realloc

--- 10-14C++/作业/栈/main.cpp
+++ 10-14C++/作业/栈/main.cpp
@@ -15,28 +15,39 @@ private:
     int size;
 
 public:
-    Stack(int size){
+    Stack(int size)
+    {
         base = (SElementType *)malloc(sizeof(SElementType) * size);
         top = base;
         size = 0;
     }
-    void GetTop(){
-        cout << "Top: " << *(top-1) << endl;
+    void GetTop()
+    {
+        cout << "Top: " << *(top - 1) << endl;
     }
     void push(SElementType s)
     {
+        if (size >= 10)
+        {
+            base = (SElementType *)realloc(base, sizeof(SElementType)*(size+1));
+            top = base + size;
+            size++;
+        }
+
         *(top++) = s;
         size++;
     }
-    void pop(){
+    void pop()
+    {
         top--;
         size--;
     }
-    void show_value(){
+    void show_value()
+    {
         SElementType *Pointer = base;
         for (int i = 0; i < size; i++)
         {
-            cout << "第" << i+1 << "个数字是:" <<  *(Pointer++) << endl;
+            cout << "第" << i + 1 << "个数字是:" << *(Pointer++) << endl;
         }
     }
 };
